6d7d8261fbde47bd928080f20e75eb36d0622a77
codedeep9 <saideep.meka123@gmail.com>
2024-08-02 13:47:24 +0530

    crud express

--- express_server/index.js
+++ express_server/index.js
@@ -1,47 +1,93 @@
-const express = require('express');
+const express = require("express");
 
-const app = express();
-const PORT = 8080;
+const fs = require("fs");
 
-const products = require('./data.json');
+const app = express(); //Handler function
 
-// routing
+const products = require("./mock_data.json");
 
-app.get('/', (req, res) => {
-    res.send('Hi welcome to the shopping app..')
+app.listen(8001, () => {
+  console.log("server is running at port 8001");
 });
 
-// hybrid api development
-app.get('/api/products',(req,res)=>{
-    res.json(products);
+// middleware
+app.use(express.urlencoded())
+app.use(express.json())
+
+// Routing
+app.get("/", (req, res) => {
+  res.send("Hi Onkar....Welcome to our demo session....");
+});
+
+// Hybrid API developement means same API can access in cross platform devices...
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+// Server side rendering - When we rendering a complete html from your server
+app.get("/product", (req, res) => {
+  const html = `<ul> ${products.map(
+    (product) => `<li> ${product.product_name} <\li>`
+  )}</ul>`;
+
+  res.send(html);
+});
+
+// Route paramters...get some data based on id
+app.get("/api/products/:id", (req, res) => {
+  // console.log(req.params.id)
+
+  const id = Number(req.params.id);
+
+  const product = products.find((x) => x.id === id);
+  return res.json(product);
+});
+
+// post put delete
+
+// Post Method
+app.post("/api/AddProduct", (req, res) => {
+  console.log(req.body)
+  const newData = req.body;
+  products.push({ id: products.length + 1, ...newData });
+  fs.writeFile("./mock_data.json", JSON.stringify(products), (err, data) => {
+    if (err) {
+        return res.send(err)
+    }
+    return res.send({ status: "Successfully created", id: products.length });
+  });
+});
+
+
+// PUT
+app.put('/api/products/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const body = req.body
+
+    const productIndex = products.findIndex((x) => x.id === id);
+
+    products[productIndex] = {id: id, ...body}
+
+    fs.writeFile("./mock_data.json", JSON.stringify(products), (err, data) => {
+        if (err) {
+            return res.send(err)
+        }
+        return res.json({ status: "Successfully updated", id: products.length });
+      });
 })
 
-// default route
-// app.get('*',(req,res)=>{
-//     res.status(404).send('page not found');
-// })
+// Delete
+app.delete('/api/DeleteProduct/:id', (req, res) => {
+    const id = Number(req.params.id)
+    
+    const productIndex = products.findIndex((x) => x.id === id);
 
-// route parameters
-app.get('/api/products/:id',(req,res)=>{
-    const id=Number(req.params.id)
-    const product=products.find((product)=>{
-        return product.id==id
-    })
-    res.send(product.product_name);
-})
-// server side rendering
+    products.splice(productIndex, 1)
 
-app.get('/products', (req, res) => {
-    const html = `<ul>${products.map((product) => {
-        return `<li>${product.product_name}</li>`
-    })}
-    </ul>`
-    res.send(html);
-})
-
-
-
-
-app.listen(PORT, () => {
-    console.log(`server running at port ${PORT}`)
+    fs.writeFile("./mock_data.json", JSON.stringify(products), (err, data) => {
+        if (err) {
+            return res.status(500).json({ status: 'error', message: "internal 500 server error" })
+        }
+        return res.json({ status: "deleted Successfully", id: products.length });
+      });
 })
